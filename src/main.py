5d7e625f1e4af9988027b0dd4ea49c7060c9618f
Timothée MARRERO MEYER <143833750+timotheeMM@users.noreply.github.com>
2023-09-14 18:28:34 +0200

    put the code in a function named download_video

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from pytube import YouTube
 
-url = input("Please enter the URL of the YouTube video you want to download: ")
+def download_video():
+    url = input("Please enter the URL of the YouTube video you want to download: ")
+    yt = YouTube(url)
+    stream = yt.streams.get_highest_resolution()
+    output_path = "downloads"
+    stream.download(output_path)
 
-yt = YouTube(url)
-
-stream = yt.streams.get_highest_resolution()
-
-output_path = "downloads"
-
-stream.download(output_path)
+if __name__ == "__main__":
+    download_video()
