317523599fcfddfbab069964eb64933305fdab20
Timothée MARRERO MEYER <143833750+timotheeMM@users.noreply.github.com>
2023-09-16 12:50:36 +0200

    add a progress bar using tqdm

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
+from tqdm import tqdm
 from pytube import YouTube
 
 def download_video(url):
     """
-    Download a video from a given YouTube URL.
+    Download a video from a given YouTube URL with a progress bar using tqdm.
 
     Args:
         url (str): The URL of the YouTube video to be downloaded.
@@ -10,10 +11,25 @@ def download_video(url):
     Returns:
         None
     """
-    
+
     yt = YouTube(url)
     stream = yt.streams.get_highest_resolution()
+
+    # Create a tqdm progress bar
+    pbar = tqdm(total=stream.filesize)
+
+    # Define a callback function to update the progress bar
+    def on_progress(stream, chunk, bytes_remaining):
+        pbar.update(len(chunk))
+
+    # Add the on_progress function to the yt instance
+    yt.register_on_progress_callback(on_progress)
+
+    # Download the video with the progress bar callback
     stream.download("downloads")
+
+    # Close the progress bar
+    pbar.close()
 
 if __name__ == "__main__":
     video_url = input("Enter the URL of the YouTube video: ")
