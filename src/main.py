9b7fd8801a6efb1391f7b834e5bb190e89f84256
Yashwanth Kumar <itsmectm@gmail.com>
2023-10-07 23:34:06 +0530

    youtube-downloader Update 1.1

--- src/main.py
+++ src/main.py
@@ -1,58 +1,80 @@
 from pytube import YouTube
-from tqdm import tqdm
+from tkinter import messagebox
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+import os
+from pytube import YouTube
 
-def download_video(url):
-    """
-    Download a video from a given YouTube URL with a progress bar using tqdm.
+PATH = os.getcwd() + "\downloads"
 
-    Args:
-        url (str): The URL of the YouTube video to be downloaded.
 
-    Returns:
-        None
-    """
+def get_path():
+    global PATH
+    PATH = filedialog.askdirectory()
+    filepath.config(text=PATH)
 
-    # Create a YouTube object for the given URL
-    yt = YouTube(url)
 
-    # Get the highest resolution stream
+def bar():
+    progress['value'] = 0
+    download_success.config(text="")
+    window.update()
+
+    url = video_url.get()
+
+    try:
+        yt = YouTube(url)
+    except:
+        messagebox.showerror(title="ERROR!", message="Provided URL is either empty or invalid, please verify and try again.")
+
+    window.update()
     stream = yt.streams.get_highest_resolution()
 
-    # Create a tqdm progress bar
-    pbar = tqdm(total=stream.filesize, unit="B", unit_scale=True, colour="red")
+    fileSize = stream.filesize
 
-    # Define a callback function to update the progress bar
     def on_progress(stream, chunk, bytes_remaining):
-        """
-        Callback function to update the progress bar.
+        progress['value'] += (100*len(chunk))/fileSize
+        window.update()
+    
 
-        Args:
-            stream (pytube.Stream): The stream being downloaded.
-            chunk (bytes): The chunk of data being downloaded.
-            bytes_remaining (int): The number of bytes remaining to download.
-
-        Returns:
-            None
-        """
-        
-        # Update the tqdm progress bar
-        pbar.update(len(chunk))
-
-    # Add the on_progress function to the yt instance
     yt.register_on_progress_callback(on_progress)
 
-    # Download the video with the progress bar callback
-    stream.download("downloads")
+    stream.download(PATH)
 
-    # Close the progress bar
-    pbar.close()
+    download_success.config(text="DOWNLOAD SUCCESS! ", font=("Arial", 18, "bold"), fg="green")
+    
 
-    # Print a message to say that the download has been successfully completed
-    print("Download successfully completed!")
 
 if __name__ == "__main__":
-    # Prompt the user to enter the URL of the YouTube video
-    video_url = input("Enter the URL of the YouTube video: ")
+    window = Tk()
+    window.title("Youtube Downloader")
+    window.minsize(450, 150)
+    
 
-    # Call the download_video function with the provided URL
-    download_video(video_url)
+    label = Label(text="Enter the URL of the YouTube video: ")
+    label.grid(row=0, column=0)
+
+    video_url = Entry(width=60)
+    video_url.focus()
+    video_url.grid(row=0, column=1)
+
+    url = video_url.get()
+    download = Button(text="Download", command=bar)
+    download.grid(row=0, column=2, padx=10, pady=10)
+
+    filepath_label = Label(text="Downloading Path: ")
+    filepath_label.grid(row=1, column=0)
+
+    filepath = Label(text=f"{PATH}")
+    filepath.grid(row=1, column=1)
+
+    browse = Button(text="Change Path", command=get_path)
+    browse.grid(row=1, column=2, padx=10, pady=10)
+
+    progress = ttk.Progressbar(window, orient=HORIZONTAL,length=430, mode='determinate')
+    progress.grid(row=2, column=0, columnspan=3,padx=20, pady=20, sticky=EW)
+
+    download_success = Label(text="")
+    download_success.grid(row=3, column=1, sticky="nw")
+
+    window.mainloop()
