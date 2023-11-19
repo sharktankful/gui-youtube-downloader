fbc63545f26e6a09b9684fe5c23b5c014dee0f2c
Timothée Marrero Meyer <143833750+timotheeMM@users.noreply.github.com>
2023-11-19 19:34:03 +0100

    remove the code of the cli version that is now in this repository: https://github.com/timotheeMM/cli-youtube-downloader

--- src/main.py
+++ src/main.py
@@ -1,25 +1,3 @@
-from downloader import Downloader
+from downloader import gui
 
-
-while True:
-    choice = input(
-        "Welcome to YouTube Downloader!\n"
-        "- Enter 1 for Command Line Interface\n"
-        "- Enter 2 for Graphical User Interface\n"
-        "- Enter 'exit' to Exit\n"
-        "Enter your input: "
-    )
-
-    if choice == "1":
-        Downloader.cli()
-
-    elif choice == "2":
-        Downloader.gui()
-
-    elif choice.lower() == "exit":
-        print("\nThank you for using Youtube Downloader. See you soon!")
-        exit()
-
-    else:
-        print(f"The choice '{choice}' is not among the available options. Please try again...\n")
-
+gui()
