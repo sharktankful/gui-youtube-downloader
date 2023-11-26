fc76dc56a4ea94e5936ec62ea97250932ddb6430
Timothée MARRERO MEYER <143833750+timotheeMM@users.noreply.github.com>
2023-11-26 09:13:52 +0100

    update downloader.py

--- src/downloader.py
+++ src/downloader.py
@@ -1,11 +1,12 @@
-import os
-import tkinter as tk
-from tkinter import ttk, filedialog
-from tkinter import ttk
-from ttkthemes import ThemedTk
 from pytube import YouTube
+import tkinter as tk
 
-# Default download path
+from tkinter import ttk, filedialog
+from ttkthemes import ThemedTk
+
+import os
+
+
 PATH = os.path.join(os.getcwd(), "downloads")
 
 def gui():
@@ -15,8 +16,13 @@ def gui():
 
     print("\nGUI running...")
 
+    path = os.path.join(os.getcwd(), "downloads/")
+
     def get_path():
-        """Open a dialog to choose the download path and update the GUI label."""
+        """
+        Open a dialog to choose the download path and update the GUI label.
+        """
+        
         global PATH
         PATH = filedialog.askdirectory()
         filepath.config(text=PATH)
@@ -57,25 +63,23 @@ def gui():
 
         yt.register_on_progress_callback(on_progress)
 
+        print(PATH)
+
         stream.download(PATH)
         download_status.config(text="DOWNLOAD SUCCESS!", fg="green")
-        
-
-            
 
     window = ThemedTk(theme="breeze")
     window.title("YouTube Downloader")
-    window.minsize(450, 150)
 
     label = tk.Label(text="Enter the URL of the YouTube video:")
-    label.grid(row=0, column=0, padx=10)
+    label.grid(row=0, column=0, padx=15, pady=10)
 
     video_url = tk.Entry(width=60)
+    video_url.grid(row=0, column=1, padx=15, pady=10)
     video_url.focus()
-    video_url.grid(row=0, column=1)
 
-    download = tk.Button(text="Download", command=download_video)
-    download.grid(row=0, column=2, padx=10, pady=10)
+    download_button = tk.Button(text="Download", command=download_video, width=25)
+    download_button.grid(row=0, column=2, padx=15, pady=10)
 
     filepath_label = tk.Label(text="Downloading Path:")
     filepath_label.grid(row=1, column=0)
@@ -83,11 +87,11 @@ def gui():
     filepath = tk.Label(text=PATH)
     filepath.grid(row=1, column=1)
 
-    browse = tk.Button(text="Change Path", command=get_path)
-    browse.grid(row=1, column=2, padx=10, pady=10)
+    browse_button = tk.Button(text="Change Path", command=get_path, width=25)
+    browse_button.grid(row=1, column=2, padx=15, pady=10)
 
-    progress = ttk.Progressbar(window, orient="horizontal", length=430, mode='determinate')
-    progress.grid(row=2, column=0, columnspan=3, padx=20, pady=20, sticky="ew")
+    progress = ttk.Progressbar(window, mode='determinate')
+    progress.grid(row=2, column=0, padx=15, pady=10, columnspan=3, sticky="ew")
 
     download_status = tk.Label(text="")
     download_status.grid(row=3, column=2, padx=15, pady=10)
