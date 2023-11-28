2177fa86fa007c6f99805dedc0ae7ecf3bdd8ff1
Timothée MARRERO MEYER <143833750+timotheeMM@users.noreply.github.com>
2023-11-28 17:20:33 +0100

    rename variable names for more readability

--- src/downloader.py
+++ src/downloader.py
@@ -25,14 +25,14 @@ def gui():
         
         global PATH
         PATH = filedialog.askdirectory()
-        filepath.config(text=PATH)
+        filepath_label.config(text=PATH)
 
     def download_video():
         """
         Download the YouTube video when the 'Download' button is clicked.
         """
 
-        url = video_url.get()
+        url = video_url_entry.get()
 
         try:
             yt = YouTube(url)
@@ -40,8 +40,8 @@ def gui():
         except:
             exit("\nThe URL you provided is either empty or invalid.")
 
-        progress['value'] = 0
-        download_status.config(text="Downloading...")
+        download_progressbar['value'] = 0
+        download_status_label.config(text="Downloading...")
         window.update()
 
         stream = yt.streams.get_highest_resolution()
@@ -58,7 +58,7 @@ def gui():
             """
 
             downloaded_bytes = file_size - bytes_remaining
-            progress['value'] = int((downloaded_bytes / file_size) * 100)
+            download_progressbar['value'] = int((downloaded_bytes / file_size) * 100)
             window.update()
 
         yt.register_on_progress_callback(on_progress)
@@ -66,35 +66,35 @@ def gui():
         print(PATH)
 
         stream.download(PATH)
-        download_status.config(text="DOWNLOAD SUCCESS!", fg="green")
+        download_status_label.config(text="DOWNLOAD SUCCESS!", fg="green")
 
     window = ThemedTk(theme="breeze")
     window.title("YouTube Downloader")
 
-    label = tk.Label(text="Enter the URL of the YouTube video:")
-    label.grid(row=0, column=0, padx=15, pady=10)
+    url_message_label = tk.Label(text="Enter the URL of the YouTube video:")
+    url_message_label.grid(row=0, column=0, padx=15, pady=10)
 
-    video_url = tk.Entry(width=60)
-    video_url.grid(row=0, column=1, padx=15, pady=10)
-    video_url.focus()
+    video_url_entry = tk.Entry(width=60)
+    video_url_entry.grid(row=0, column=1, padx=15, pady=10)
+    video_url_entry.focus()
 
     download_button = tk.Button(text="Download", command=download_video, width=25)
     download_button.grid(row=0, column=2, padx=15, pady=10)
 
-    filepath_label = tk.Label(text="Downloading Path:")
-    filepath_label.grid(row=1, column=0)
+    filepath_message_label = tk.Label(text="Downloading Path:")
+    filepath_message_label.grid(row=1, column=0)
 
-    filepath = tk.Label(text=PATH)
-    filepath.grid(row=1, column=1)
+    filepath_label = tk.Label(text=PATH)
+    filepath_label.grid(row=1, column=1)
 
     browse_button = tk.Button(text="Change Path", command=get_path, width=25)
     browse_button.grid(row=1, column=2, padx=15, pady=10)
 
-    progress = ttk.Progressbar(window, mode='determinate')
-    progress.grid(row=2, column=0, padx=15, pady=10, columnspan=3, sticky="ew")
+    download_progressbar = ttk.Progressbar(window, mode='determinate')
+    download_progressbar.grid(row=2, column=0, padx=15, pady=10, columnspan=3, sticky="ew")
 
-    download_status = tk.Label(text="")
-    download_status.grid(row=3, column=2, padx=15, pady=10)
+    download_status_label = tk.Label(text="")
+    download_status_label.grid(row=3, column=2, padx=15, pady=10)
 
     window.mainloop()
 
