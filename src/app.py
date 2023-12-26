9f0418966e42305fa43ca468aad305851a2ed118
Timothée MARRERO MEYER <143833750+timotheemm@users.noreply.github.com>
2023-12-26 13:53:28 +0100

    add a tabview to switch from audio mode to video mode

--- src/app.py
+++ src/app.py
@@ -8,6 +8,8 @@ from tkinter import filedialog, messagebox
 
 import customtkinter
 
+from tabview import TabView
+
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("green")
 
@@ -19,7 +21,7 @@ class App(customtkinter.CTk):
 
     def __init__(self):
         """
-        Initialize the application window and set up the UI components.
+        Initialize the application window.
         """
 
         super().__init__()
@@ -28,57 +30,22 @@ class App(customtkinter.CTk):
 
         self.title("YouTube Downloader")
 
-        self.grid_columnconfigure((0, 1, 2), weight=0)
-        self.grid_rowconfigure((0, 1, 2, 3), weight=1)
+        self.tabview = TabView(master=self)
+        self.tabview.grid(row=0, column=0, padx=20, pady=30)
 
-        # line 1
-
-        self.url_status_label = customtkinter.CTkLabel(self, text="Enter the URL of the YouTube video:")
-        self.url_status_label.grid(row=0, column=0, padx=15, pady=10)
-
-        self.url_entry = customtkinter.CTkEntry(self, width=500)
-        self.url_entry.grid(row=0, column=1, padx=15, pady=10)
-
-        self.download_button = customtkinter.CTkButton(self, text="Download", width=150)
-        self.download_button.grid(row=0, column=2, padx=15, pady=10)
-        self.download_button.configure(command=self._download_video)
-
-        # line 2
-
-        self.path_label1 = customtkinter.CTkLabel(self, text="Downloading Path:")
-        self.path_label1.grid(row=1, column=0, padx=15, pady=10)
-
-        self.path_label2 = customtkinter.CTkLabel(self, text=self.download_path)
-        self.path_label2.grid(row=1, column=1, padx=15, pady=10)
-
-        self.change_path_button = customtkinter.CTkButton(self, text="Change path", width=150)
-        self.change_path_button.grid(row=1, column=2, padx=15, pady=10)
-        self.change_path_button.configure(command=self._change_path)
-
-        # line 3
-
-        self.progress_bar = customtkinter.CTkProgressBar(self, mode="determinate")
-        self.progress_bar.grid(row=2, column=0, padx=15, pady=10, columnspan=3, sticky="ew")
-        self.progress_bar.set(0)
-
-        # line 4
-
-        self.status_label = customtkinter.CTkLabel(self, text="Status")
-        self.status_label.grid(row=3, column=2, padx=15, pady=10)
-
-    def _download_video(self):
+    def download_video(self):
         """
         Download the YouTube video specified by the provided URL.
         """
 
-        url = self.url_entry.get()
+        url = self.tabview.url_entry.get()
 
         try:
             video = YouTube(url)
 
             stream = video.streams.get_highest_resolution()
 
-            self.status_label.configure(text="Downloading...")
+            self.tabview.status_label.configure(text="Downloading...")
 
             def on_progress(stream, chunk, bytes_remaining):
                 """
@@ -91,20 +58,20 @@ class App(customtkinter.CTk):
                 """
 
                 downloaded_bytes = stream.filesize - bytes_remaining
-                self.progress_bar.set(downloaded_bytes / stream.filesize)
-                self.progress_bar.update()
+                self.tabview.progress_bar.set(downloaded_bytes / stream.filesize)
+                self.tabview.progress_bar.update()
 
             video.register_on_progress_callback(on_progress)
 
             stream.download(self.download_path)
 
-            self.status_label.configure(text="DOWNLOAD SUCCESS!")
-            self.status_label.update()
+            self.tabview.status_label.configure(text="DOWNLOAD SUCCESS!")
+            self.tabview.status_label.update()
 
             time.sleep(3)
 
-            self.progress_bar.set(0)
-            self.status_label.configure(text="Status")
+            self.tabview.progress_bar.set(0)
+            self.tabview.status_label.configure(text="Status")
 
         except RegexMatchError:
             messagebox.showerror(
@@ -118,7 +85,7 @@ class App(customtkinter.CTk):
                 message="The provided video is unavailable."
             )
 
-    def _change_path(self):
+    def change_path(self):
         """
         Change the download path by opening a directory selection dialog.
         """
@@ -128,5 +95,5 @@ class App(customtkinter.CTk):
         if self.download_path == "":
             self.download_path = os.path.join(os.getcwd(), "downloads")
 
-        self.path_label2.configure(text=self.download_path)
-        self.path_label2.update()
+        self.tabview.path_label2.configure(text=self.download_path)
+        self.tabview.path_label2.update()
